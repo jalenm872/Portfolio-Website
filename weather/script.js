c14da6cfac086b729590d32d996e29ee0291d339
Jalen Mendoza <54971623+jalenm872@users.noreply.github.com>
2023-09-14 10:07:12 -0400

    Fixed navigator.geolocation undefined return

--- weather/script.js
+++ weather/script.js
@@ -1,11 +1,31 @@
 
 // If the user allows the location access, run the sucessCallback function
 const sucessCallback = (position) => {
-    console.log(position);
+    // console.log(position);
     let lat = position.coords.latitude;
     let lon = position.coords.longitude; 
-    //Run the getWeather function
-    getWeather(lat, lon);
+    //Get the name of the city
+    fetch(
+        "http://api.openweathermap.org/geo/1.0/reverse?lat="+lat+"&lon="+lon+"&limit=1&appid=b222be32564b5a976119b9aed1c84a8d"
+    )
+    .then(response => {
+        // handle the response
+        if(response.ok) {
+            return response.json();
+        } else {
+            throw new Error("Something went wrong");
+        }
+    })
+    .then(data => {
+        // handle the data
+        // console.log(data);
+        let city = data[0].name
+        let state = data[0].state;
+        let country = data[0].country;
+        let name = city + ", " + state + ", " + country;
+        //Run the getWeather function
+        getWeather(lat, lon, name);
+    })
 }
 
 // If the user denies the location access, run the errorCallback function
@@ -59,17 +79,17 @@ function getWeather(lat, lon, name) {
     })
     .then(data => {
         // handle the data
-        console.log(data);
+        // console.log(data);
         let temp = data.current.temp;
         temp = Math.round(temp);
-        let discription = data.current.weather[0].description;
+        let description = data.current.weather[0].description;
         let humidity = data.current.humidity;
         let windSpeed = data.current.wind_speed;
-        console.log(name, temp, discription, humidity, windSpeed);
+        // console.log(name, temp, description, humidity, windSpeed);
         // Update the weather data
         document.querySelector(".city-name").innerText = name;
         document.querySelector(".city-temp").innerText = temp + "°C";
-        document.querySelector(".city-description").innerText = discription;
+        document.querySelector(".city-description").innerText = description;
         document.querySelector(".humidity").innerText = "Humidity: " + humidity + "%";
         document.querySelector(".wind").innerText = windSpeed + " km/h";
         document.querySelector(".search-bar").value = "";
@@ -97,6 +117,7 @@ function updateWeather(){
         }
     })
     .then(data => {
+        // console.log(data);
         // handle the data
         let lat = data[0].lat;
         let lon = data[0].lon;
